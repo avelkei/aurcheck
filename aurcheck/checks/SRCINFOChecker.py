ed67d80cfef186b83e434d88f72eef417fdb5512
Adam Velkei <adam@avelkei.eu>
2020-08-22 21:17:55 +0200

    Rewrote URL comparison

--- aurcheck/checks/SRCINFOChecker.py
+++ aurcheck/checks/SRCINFOChecker.py
@@ -7,20 +7,44 @@ class SRCINFOChecker(BaseChecker):
 		import os.path
 		import re
 
-		if not os.path.isfile(os.path.join(self.package_path, "PKGBUILD")) or not os.path.isfile(os.path.join(self.package_path, ".SRCINFO")):
+		if not os.path.isfile(os.path.join(self.package_path, "PKGBUILD")) \
+			or not os.path.isfile(os.path.join(self.package_path, ".SRCINFO")):
 			self.fail_message = "file missing, can't check .SRCINFO"
 			return
 
-		pkgbuild_contents = open(os.path.join(self.package_path, "PKGBUILD")).read()
-		srcinfo_contents = open(os.path.join(self.package_path, ".SRCINFO")).read()
+		try:
+			pkgbuild_contents = open(os.path.join(self.package_path, "PKGBUILD")).read()
+		except UnicodeDecodeError:
+			pkgbuild_contents = open(os.path.join(self.package_path, "PKGBUILD"), encoding="ISO-8859-1").read()
+		
+		try:
+			srcinfo_contents = open(os.path.join(self.package_path, ".SRCINFO")).read()
+		except UnicodeDecodeError:
+			srcinfo_contents = open(os.path.join(self.package_path, ".SRCINFO"), encoding="ISO-8859-1").read()
 
+		# Find variables in PKGBUILD
+		pkgbuild_vars = {}
+		bash_var_pattern = re.compile(r"([_a-zA-Z0-9]+)=([\"\'].+[\"\']|[^\s]+)")
+		for line in pkgbuild_contents.splitlines():
+			match = bash_var_pattern.match(line.strip())
+			if match is not None:
+				pkgbuild_vars[match.group(1)] = match.group(2)
+
+		# Substitute variables with their values in PKGBUILD contents
+		# (where they're not bash formatted expressions)
+		for (var_name, var_value) in pkgbuild_vars.items():
+			pkgbuild_contents = re.sub(rf"(\${var_name}|\$\{{{var_name}\}})", var_value.strip("'\""), pkgbuild_contents)
+		
 		# Check if there are any URLs in the PKGBUILD file that are not present in .SRCINFO
-		# https://stackoverflow.com/a/3809435
-		url_regex = re.compile(r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)")
+		url_regex = re.compile(r"(https?:\/\/[^\[\]\(\)\s'\"]+)")
 
 		pkgbuild_urls = url_regex.findall(pkgbuild_contents)
 		srcinfo_urls = url_regex.findall(srcinfo_contents)
 
+		# Remove duplicates
+		pkgbuild_urls = list(set(pkgbuild_urls))
+
 		for url in pkgbuild_urls:
 			if url not in srcinfo_urls:
-				self.add_result_item(CheckResult(severity=Severity.WARN, message=f"PKGBUILD contains a URL that isn't found in .SRCINFO: {url}"))
+				self.add_result_item(CheckResult(severity=Severity.WARN, 
+					message=f"PKGBUILD contains URL which can't be found in .SRCINFO: {url}"))
